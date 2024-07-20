b565daf8b2a92d195ea34dc78fa4b03e1231907f
Jack Xu <jackxxu@gmail.com>
2024-07-20 17:18:12 -0400

    run improvements
    Former-commit-id: 9d2e19e6a5e7c74977d1d5daef2f6f6bd1157ac4 [formerly ca0b717d1b33556283abb308980daba1968d6c75]
    Former-commit-id: 3344e7d8e0d877c3519d2dee57190fe03fccffc3

--- test_run.py
+++ test_run.py
@@ -18,27 +18,23 @@ def write_video_to_frames(row):
     video_path = row["file"]
     cap = cv2.VideoCapture(video_path)
     frame_num = 0
-    start_frame, end_frame = row["begin frame"], row["end frame"]
-
+    start_frame, end_frame = int(row[" begin frame"]), int(row[" end frame"])
     # max frames in a mp4 is 1000
     if end_frame - start_frame > 1000:
         end_frame = start_frame + 1000
 
-    while True:
+    # write the frames as individual images to the frames directory 
+    while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
+
+        if frame_num >= start_frame and frame_num < end_frame:
+            cv2.imwrite(f"frames/{frame_num}.jpg", frame)
         frame_num += 1
-        frame_path = f"frames/{video_path}_{frame_num}.jpg"
-        cv2.imwrite(frame_path, frame)
-        logging.info(f"Saved frame {frame_num} to {frame_path}")
     cap.release()
     return
 
 
 write_video_to_frames(dataset.iloc[0])
 
-
-# %%
-dataset
-# %%
