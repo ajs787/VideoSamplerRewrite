050423359db4674aa6126391d90839a00b324455
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-20 16:55:32 -0400

    remove spawn
    Former-commit-id: 31bf1f196fa2ce69a765a3dbfddaf17e806d9a8c [formerly 23b668c19c628b7dfde8c9646fcab8fd933f8a51]
    Former-commit-id: bdb5001a460082f9f6f85c46364cb0353409b577

--- Dataprep.py
+++ Dataprep.py
@@ -29,7 +29,7 @@ logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 # logging.info(f"RLIMIT_NOFILE: {resource.getrlimit(resource.RLIMIT_NOFILE)}")
 
 
-multiprocessing.set_start_method("spawn", force=True)
+# multiprocessing.set_start_method("spawn", force=True)
 # os.environ["OMP_NUM_THREADS"] = "1"
 
 
