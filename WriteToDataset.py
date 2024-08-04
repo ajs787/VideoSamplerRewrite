2c9daf1ff15bee16e774cabf214a791c5c80d300
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-08-04 16:29:29 -0400

    up speeds
    Former-commit-id: 971e0a09540d745d66a7c9023d870d53487b04e5 [formerly 01dca4f42b999ea068acc655f6e8b33b7d2de05c]
    Former-commit-id: 26e9ff9e31cf37e715f8f8506a35b9e69e6539c0

--- WriteToDataset.py
+++ WriteToDataset.py
@@ -102,7 +102,7 @@ def write_to_dataset(
     frames_per_sample: int = 1,
     out_channels: int = 1,
     batch_size: int = 10,
-    num_workers: int = 15,
+    num_workers: int = 20,
 ):
     """
     Writes samples from a directory to a dataset tar file.
